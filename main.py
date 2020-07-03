80c7697ed6931506cf26379e55a98c9216f68afb
martamv <martamamvel@hotmail.com>
2020-07-03 10:51:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 
 from src.tf.estimator import Estimator
 from src.tf.classifier import Classifier
-from src.tf2.classifier import Classify
-from src.tf2.classifier2 import Classify2
+from src.tf2.classifier import Classify      #Import this classifier if you want to use 'outcome' as the diagnostic parameter
+from src.tf2.classifier2 import Classify2    #Import this classifier if you want to use the alpha angle as the diagnostic parameter
 import tensorflow as tf
 
 if __name__ == '__main__':
-   #estimator = Estimator(num_steps = 20000, batch_size=128, display_step=1000, save_step = 5000, doRestore = False)
-   #classifier = Classifier(num_steps = 50000, batch_size=32, display_step=1000, save_step = 10000, doRestore = False) 
-   Classify2()
-   #try:
-   		#with tf.device('/device:GPU:1'):
-   			#Classify()
-   #except RuntimeError as e:
-  		#print(e)
+   #Classify()
+   Classify2()    #in this case we will be using alpha
